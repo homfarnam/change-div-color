2b8fad78b5868a3f04e0c869d18998983f4da374
homfarnam <homfarnam@gmail.com>
2020-12-08 10:30:18 +0330

    Change div color after click button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [colorInput, setColorInput] = useState<string>("");
+  const [color, setColor] = useState<string>("");
+
+  const inputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setColorInput(e.target.value);
+  };
+  const changeColor = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    const mycolor = document.getElementsByTagName("input")[0].value;
+    setColor(mycolor);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        Change Div color
+        <div>
+          <input
+            type="text"
+            placeholder="Enter the color name"
+            value={colorInput}
+            onChange={inputOnChange}
+            id="newcolor"
+          />
+          <button type="submit" onClick={changeColor}>
+            Submit
+          </button>
+          <div
+            className="colorfull"
+            style={{ backgroundColor: `${color}` }}
+          ></div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
